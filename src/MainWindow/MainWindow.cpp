4c265fa4737a8d9e6ebbc187163d2ba0a6e6d93f
tan <3262975463@qq.com>
2024-12-20 20:30:26 +0800

    feat:调整样式

--- src/MainWindow/MainWindow.cpp
+++ src/MainWindow/MainWindow.cpp
@@ -8,8 +8,11 @@ MainWindow::MainWindow(QWidget *parent)
 {
     ui->setupUi(this);
 
-    //ui->file->Init("D://code//TanCloudPc//src//MainWindow//icon//folder_common.svg", "qrc:/icon/file.png", u8"文件");
-    ui->file->Init(":/MainWindow/folder_common.svg", ":/MainWindow/folder_selected.svg", u8"文件");
+    ui->file->init(":/MainWindow/folder_common.svg", ":/MainWindow/folder_selected.svg", u8"文件");
+    ui->transfer->init(":/MainWindow/transfer_common.svg", ":/MainWindow/transfer_selected.svg", u8"传输");
+    ui->share->init(":/MainWindow/share_common.svg", ":/MainWindow/share_selected.svg", u8"分享");
+    ui->setting->init(":/MainWindow/setting_common.svg", ":/MainWindow/setting_common.svg", u8"");
+    ui->setting->setIconSize(20, 20);
 }
 
 MainWindow::~MainWindow()
